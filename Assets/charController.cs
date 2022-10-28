aa96fed8324ec23840c51caaa3c85c15fa07742d
TeriZhang <55502531+TeriZhang@users.noreply.github.com>
2022-10-28 14:15:04 -0700

    ?

---
+++ Assets/charController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charController : MonoBehaviour
+{
+    public Vector2 moveDir;
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputs();
+    }
+
+    private void FixedUpdate()
+    {
+        move();
+    }
+
+    void inputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY).normalized;
+    }
+    void move()
+    {
+        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+    }
+}
