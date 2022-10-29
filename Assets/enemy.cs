7ee6b6407c7a09835213f30f8783c628952c0166
babyySoup <71286763+babyySoup@users.noreply.github.com>
2022-10-29 15:15:25 -0700

    Revert "1"
    This reverts commit de18905cbc89239716713a4be9752347cd83a70b, reversing
    changes made to d12009c7df8b20361907336747f62c598e6c7f13.

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
-    public float speed;
-    public int currentSpot;
-    public int totalSpot;
-    public float waitTime;
-    public List<GameObject> spots;
-
-    public GameObject altered, talking;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,27 +13,6 @@ public class enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, spots[currentSpot].transform.position, Time.deltaTime * speed);
-
-        if (Vector2.Distance(transform.position, spots[currentSpot].transform.position) <0.2f)
-        {
-            currentSpot += 1;
-            if(currentSpot > totalSpot)
-            {
-                currentSpot = 0;
-            }
-        }
-    }
-
-    public void Stop()
-    {
-        speed = 0;
-        talking.SetActive(true);
-    }
-
-    public void Resume()
-    {
-        speed = 3;
-        talking.SetActive(false);
+        
     }
 }
