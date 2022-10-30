5385501f3fc5e1fab91db584a6d9d85a04623dc8
QIHUAN1 <qjiang1@inside.artcenter.edu>
2022-10-29 19:53:56 -0700

    1

---
+++ Assets/Scripts/Menu,UI/Turtorial.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtorial : MonoBehaviour
+{
+    public List<GameObject> ttt;
+    public int current;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextSlide()
+    {
+        current += 1;
+        
+
+        for (int i = 0; i < 6; i++)
+        {
+            ttt[i].gameObject.SetActive(false);
+            ttt[current].gameObject.SetActive(true);
+        }
+
+    }
+}
